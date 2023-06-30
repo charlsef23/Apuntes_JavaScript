e9a64c32339a76299fbf751c72f80e9f3ee2ff79
Carlos Esteve <charlsef66@gmail.com>
2023-06-30 21:37:36 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,25 @@
+/*
+const edad = 17
+const tieneCarnet = true
+const puedeConducir = edad >= 18 && tieneCarnet
+
+if (puedeConducir) {
+  console.log('Puedes conducir')
+} else {
+  console.log('No puedes conducir')
+}
+*/
+
+/*
+let cuentaAtras = 10
+
+while (cuentaAtras > 0) {
+  let revisionesRealizadas = 3
+  console.log(revisionesRealizadas)
+  cuentaAtras = cuentaAtras - 1
+}
+
+console.log(revisionesRealizadas) // -> ERROR: ReferenceError
+*/
+
+let 
